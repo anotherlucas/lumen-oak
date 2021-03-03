f57e696dafe41ec974b7124c22515431c6aa4a8e
Lucas Lemos <lucaslemos@ae.studio>
2021-03-03 14:36:11 -0500

    Updated metadata

--- pages/about.tsx
+++ pages/about.tsx
@@ -7,6 +7,10 @@ export default function AboutPage() {
         <title>About</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"></meta>
+        <meta property="og:image" content="https://www.lucaslemos.io/images/share.png" />
+        <meta property='og:description' 
+          content="Wharton-educated Software Architect with 20,000+ hours of development experience, and your new technology ally - let's build something together."
+        />
       </Head>
       <></>
     </>
--- pages/design.tsx
+++ pages/design.tsx
@@ -12,7 +12,11 @@ export default function PortfolioPage() {
       <Head>
         <title>Design</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <meta property="og:image" content="https://www.lucaslemos.io/images/share.png" />
+        <meta property='og:description' 
+          content="Wharton-educated Software Architect with 20,000+ hours of development experience, and your new technology ally - let's build something together."
+        />
       </Head>
 
       <div style={{ overflow: 'hidden' }}>
