b37b2722806c646c09bc7db0dca41292d1433a05
Lucas Lemos <lucaslemos@ae.studio>
2021-03-03 14:09:25 -0500

    Added metatag for sharing images

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,7 @@ export default function HomePage() {
         <title>Consulting</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"></meta>
+        <meta property="og:image" content="https://www.lucaslemos.io/images/share.png"></meta>
       </Head>
 
       <>
